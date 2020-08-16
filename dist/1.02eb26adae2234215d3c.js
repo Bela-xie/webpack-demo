13759076e394a4302f09c5a146d9589cbd45b274
bela <1981253197@qq.com>
2020-08-16 12:13:02 +0800

    remove woshi

--- dist/1.02eb26adae2234215d3c.js
+++ dist/1.02eb26adae2234215d3c.js
@@ -1,11 +1,2 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([
-    [1], {
-        9: function (n, o, c) {
-            "use strict";
-            c.r(o), o.default = function () {
-                console.log("懒加载")
-            }
-        }
-    }
-]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{9:function(n,o,c){"use strict";c.r(o),o.default=function(){console.log("我是懒加载")}}}]);
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvbGF6eS5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoid0ZBQUEsT0FBZSxxQkFDWEEsUUFBUUMsSUFBSSIsImZpbGUiOiIxLjAyZWIyNmFkYWUyMjM0MjE1ZDNjLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gKCkge1xuICAgIGNvbnNvbGUubG9nKCfmiJHmmK/mh5LliqDovb0nKVxufSJdLCJzb3VyY2VSb290IjoiIn0=
